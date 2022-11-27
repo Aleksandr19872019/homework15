a85d6f59f4e69192e047950833cb285ae808f758
Aleksandr19872019 <kazanev1987@mail.ru>
2022-11-27 13:21:08 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    println("Hello World!")
+
+   //Программа расчёта площади параллелипипеда
+    println("First side")
+    val side1 = readln().toInt()
+    println("Second side")
+    val side2 = readln().toInt()
+    println("Third side")
+    val side3 = readln().toInt()
+
+
+    val volume = side1 * side2 * side3
+    println("Result: ")
+    println(volume)
+
+
+
+}
